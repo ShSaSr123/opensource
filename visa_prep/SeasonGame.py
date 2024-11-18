eed03096117e389bcbe86f19a8b2150142de06d2
ShSaSr123 <148196923+ShSaSr123@users.noreply.github.com>
2024-11-18 19:23:16 +0530

    Create SeasonGame.py

---
+++ visa_prep/SeasonGame.py
@@ -0,0 +1,11 @@
+s=int(input())
+if 3<=s<=5:
+    print("Spring")
+elif 6<=s<=8:
+    print("Summer")    
+elif 9<=s<=11:
+    print("Autumn")    
+elif s==12 or s==1 or s==2:
+    print("Winter")    
+else:
+    print("Invalid")
